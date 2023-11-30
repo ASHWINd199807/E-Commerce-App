82f5f5ae57e562eb31cb2a12743cb3a0aeaac39a
ASHWINd199807 <126446593+ASHWINd199807@users.noreply.github.com>
2023-11-30 16:43:41 +0530

    Add files via upload

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,132 +1,50 @@
-import React, { useContext, useState } from "react";
-import axios from "axios";
-import { useCookies } from "react-cookie";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
+
 import "./styles.css";
-import { UserErrors } from "../../models/errors";
-import { IShopContext, ShopContext } from "../../context/shop-context";
+import { useGetProducts } from "../../hooks/useGetProducts";
+import { IProduct } from "../../models/interfaces";
 
-export const AuthPage = () => {
-  return (
-    <div className="auth">
-      <Register />
-      <Login />
-    </div>
-  );
-};
+export const CheckoutPage = () => {
+  const { getCartItemCount, getTotalCartAmount, checkout } =
+    useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
 
-const Login = () => {
-  const [_, setCookies] = useCookies(["access_token"]);
-  const { setIsAuthenticated } = useContext<IShopContext>(ShopContext);
-
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const { products } = useGetProducts();
 
   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const result = await axios.post("http://localhost:3001/auth/login", {
-        username,
-        password,
-      });
-
-      setCookies("access_token", result.data.token);
-      window.localStorage.setItem("userID", result.data.userID);
-      setIsAuthenticated(true);
-      navigate("/");
-    } catch (err) {
-      let errorMessage: string = "";
-      switch (err.response.data.type) {
-        case UserErrors.USERNAME_ALREADY_EXISTS:
-          errorMessage = "User already exists";
-          break;
-        case UserErrors.WRONG_CREDENTIALS:
-          errorMessage = "Wrong username/password combination";
-          break;
-        default:
-          errorMessage = "Something went wrong";
-      }
-
-      alert("ERROR: " + errorMessage);
-    }
-  };
-
   return (
-    <div className="auth-container">
-      <form onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {products.map((product: IProduct) => {
+          if (getCartItemCount(product._id) !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
 
-const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await axios.post("http://localhost:3001/auth/register", {
-        username,
-        password,
-      });
-      alert("Registration Completed! Now login.");
-    } catch (err) {
-      if (err.response.data.type === UserErrors.NO_USER_FOUND) {
-        alert("ERROR: No user found");
-      } else {
-        alert("ERROR: Something went wrong");
-      }
-    }
-  };
-
-  return (
-    <div className="auth-container">
-      <form onSubmit={handleSubmit}>
-        <h2>Register</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button
+            onClick={() => {
+              checkout(localStorage.getItem("userID"));
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
     </div>
   );
 };
