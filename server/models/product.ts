ccb3ffdda515d2382de7e227e65ff0a428bc93aa
ASHWINd199807 <126446593+ASHWINd199807@users.noreply.github.com>
2023-12-01 15:48:01 +0530

    Add files via upload

--- server/models/product.ts
+++ server/models/product.ts
@@ -1,19 +1,86 @@
-import { Schema, model } from "mongoose";
+import { Router, Request, Response } from "express";
+import { ProductErrors } from "../common/errors";
 
-export interface IProduct {
-  productName: string;
-  price: number;
-  description: string;
-  stockQuantity: number;
-  imageURL: string;
-}
+const router = Router();
+import { ProductModel } from "../models/product";
+import { UserModel } from "../models/user";
+import { verifyToken } from "./user";
 
-const ProductSchema = new Schema<IProduct>({
-  productName: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, required: true },
-  stockQuantity: { type: Number, required: true },
-  imageURL: { type: String, required: true },
+router.get("/", async (_, res: Response) => {
+  const products = await ProductModel.find({});
+
+  res.json({ products });
 });
 
-export const ProductModel = model<IProduct>("product", ProductSchema);
+router.post("/checkout", verifyToken, async (req: Request, res: Response) => {
+  const { customerID, cartItems } = req.body;
+  try {
+    const user = await UserModel.findById(customerID);
+
+    const productIDs = Object.keys(cartItems);// this will give an array of the ids of the products in cartitems
+    const products = await ProductModel.find({ _id: { $in: productIDs } }); // here we find products (in db) whose _id is in products
+                             //products having id , that is inside the product id
+    if (!user) {
+      return res.status(400).json({ type: ProductErrors.NO_USERS_FOUND });
+    }
+    if (products.length !== productIDs.length) {
+      return res.status(400).json({ type: ProductErrors.NO_PRODUCT_FOUND });
+    }
+
+    let totalPrice = 0;
+    for (const item in cartItems) {
+      const product = products.find((product) => String(product._id) === item);
+      if (!product) {
+        return res.status(400).json({ type: ProductErrors.NO_PRODUCT_FOUND });
+      }
+
+      if (product.stockQuantity < cartItems[item]) {
+        return res.status(400).json({ type: ProductErrors.NOT_ENOUGH_STOCK }); // check for availability
+      }
+
+      totalPrice += product.price * cartItems[item];
+    }
+
+    if (user.availableMoney < totalPrice) {
+      return res.status(400).json({ type: ProductErrors.NO_AVAILABLE_MONEY }); // check for avaible money
+    }
+
+    user.availableMoney -= totalPrice;// from here the transcation will happen  , in this line-> we are updating tthe money
+    user.purchasedItems.push(...productIDs); // updating the PURCHASED items LIST 
+
+    await user.save();
+    await ProductModel.updateMany(
+      { _id: { $in: productIDs } },
+      { $inc: { stockQuantity: -1 } } // UPDATING THE STOCK QUANTITY
+    );
+
+    res.json({ purchasedItems: user.purchasedItems });
+  } catch (error) {
+    console.log(error);
+  }
+});
+// API to get the purchased items per customer
+router.get(
+  "/purchased-items/:customerID",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    const { customerID } = req.params;
+    try {
+      const user = await UserModel.findById(customerID);
+
+      if (!user) {
+        return res.status(400).json({ type: ProductErrors.NO_USERS_FOUND });
+      }
+
+      const products = await ProductModel.find({
+        _id: { $in: user.purchasedItems },  // finding products whose _id is  in the user.purchaseditems id
+      });
+
+      res.json({ purchasedItems: products });
+    } catch (error) {
+      res.status(400).json({ type: ProductErrors.NO_USERS_FOUND });
+    }
+  }
+);
+
+export { router as productRouter };
--- server/models/user.ts
+++ server/models/user.ts
@@ -1,20 +1,74 @@
-import { Schema, model } from "mongoose";
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-export interface IUser {
-  _id?: string;
-  username: string;
-  password: string;
-  availableMoney: number;
-  purchasedItems: string[];
-}
+const router = express.Router();
+import { IUser, UserModel } from "../models/user";
+import { UserErrors } from "../common/errors";
 
-const UserSchema = new Schema<IUser>({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  availableMoney: { type: Number, default: 5000 },
-  purchasedItems: [
-    { type: Schema.Types.ObjectId, ref: "product", default: [] }, // type is objectID REFERENCING ANOTHER COLLECTION , 2:23:00
-  ],
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ username });
+    if (user) {
+      return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+    res.json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ type: err });
+  }
 });
 
-export const UserModel = model<IUser>("user", UserSchema);
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user: IUser = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ type: UserErrors.WRONG_CREDENTIALS });
+    }
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, userID: user._id });
+  } catch (err) {
+    res.status(500).json({ type: err });
+  }
+});
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, "secret", (err) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+router.get("/available-money/:userID", verifyToken, async (req, res) => {
+  const { userID } = req.params;
+
+  try {
+    const user = await UserModel.findById(userID);
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+
+    res.json({ availableMoney: user.availableMoney }); // getting the avaible money
+  } catch (err) {
+    res.status(500).json({ type: err });
+  }
+});
+
+export { router as userRouter };
