92f4d2b3f80a6df881e557e6766961bba05ca7bb
ASHWINd199807 <126446593+ASHWINd199807@users.noreply.github.com>
2023-12-01 15:47:37 +0530

    Add files via upload

---
+++ server/models/user.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+export interface IUser {
+  _id?: string;
+  username: string;
+  password: string;
+  availableMoney: number;
+  purchasedItems: string[];
+}
+
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  availableMoney: { type: Number, default: 5000 },
+  purchasedItems: [
+    { type: Schema.Types.ObjectId, ref: "product", default: [] }, // type is objectID REFERENCING ANOTHER COLLECTION , 2:23:00
+  ],
+});
+
+export const UserModel = model<IUser>("user", UserSchema);
